934f4a4aef315c678d1373d4ba81f6cc1211d780
jackpbauer-work <124083011+jackpbauer-work@users.noreply.github.com>
2023-04-17 22:50:49 -0400

    changes made to models file

---
+++ lib/models/evil_robot_story.py
@@ -0,0 +1,23 @@
+evil_robot_intro = """As your robot comes to life, Its movements are jerky and unpredictable, and its voice has an eerie tone. 
+You realize that before you can implement your plan of taking over the world with your robot companion, you need to customize your robot a bit more."""
+
+
+evil_robot_ending = """ At last, your robot becomes more and more advanced, more deadly, and more precise. You can feel the power coursing through your creation, and it's both thrilling and terrifying at the same time.
+(ROBOT NAME) is sinister looking (COLOR) with (Intelligent, Strong, or Weaponized) skills that can morph into an (ANIMAL) and Tell One Really Bad Joke. 
+
+Eventually, your robot begins to replicate itself, creating an army of mechanical minions that will do its bidding. They are ruthless and efficient, taking down anyone in their way. The world is chaotic as your robots wreak havoc on everything in sight.
+Ultimately, you stand at the head of a new world order, ruling over a planet conquered by your creations. As you look out over the devastated landscape you have created, you can't help but feel a sense of satisfaction. You have achieved what so few have ever achieved - true global domination.
+Each day, your robot becomes increasingly advanced and its weapons more deadly."""
+
+evil_end = """
+▄▄▄█████▓ ██░ ██ ▓█████    ▓█████  ███▄    █ ▓█████▄ 
+▓  ██▒ ▓▒▓██░ ██▒▓█   ▀    ▓█   ▀  ██ ▀█   █ ▒██▀ ██▌
+▒ ▓██░ ▒░▒██▀▀██░▒███      ▒███   ▓██  ▀█ ██▒░██   █▌
+░ ▓██▓ ░ ░▓█ ░██ ▒▓█  ▄    ▒▓█  ▄ ▓██▒  ▐▌██▒░▓█▄   ▌
+  ▒██▒ ░ ░▓█▒░██▓░▒████▒   ░▒████▒▒██░   ▓██░░▒████▓ 
+  ▒ ░░    ▒ ░░▒░▒░░ ▒░ ░   ░░ ▒░ ░░ ▒░   ▒ ▒  ▒▒▓  ▒ 
+    ░     ▒ ░▒░ ░ ░ ░  ░    ░ ░  ░░ ░░   ░ ▒░ ░ ▒  ▒ 
+  ░       ░  ░░ ░   ░         ░      ░   ░ ░  ░ ░  ░ 
+          ░  ░  ░   ░  ░      ░  ░         ░    ░    
+                                              ░      
+"""
---
+++ lib/models/good_robot_story.py
@@ -0,0 +1,29 @@
+good_robot_intro = """You spend hours poring your heart into your code, carefully crafting the robot's programming. 
+You decide you want your creation to use its abilities for good, so you program it to prioritize helping humanity.
+Before your robot comes to life, you want to ensure that its programming is optimized for maximum effectiveness and has all the necessary features to accomplish its goals.
+You decide to take some time to customize the robot a bit further…"""
+
+good_robot_ending = """ Great! Your (INTELLIGENT, STRONG, or LIKABLE) (COLOR) robot will be able to morph into an (ANIMAL) and (BUST INTO DANCE SONG, OBSESS OVER FOOD/DRINK, or TELL BAD DAD JOKES). 
+You can imagine your robot being a helpful and approachable companion to anyone interacting with it. 
+After spending weeks working on your robot, pouring all your time and energy into perfecting its design and programming, it’s finally finished; you activate the robot and watch as it comes to life before your eyes. You can't help but feel a sense of pride and satisfaction as you watch your creation move and speak.
+
+// INSERT ASCII IMAGE OF ROBOT HERE
+
+As the days go by, you find yourself becoming more and more attached to your robot. Its the quirkiness and helpful nature make it an indispensable assistant as you continue to learn and grow as a programmer.
+Eventually, news of your robot's incredible abilities and charming personality begins to spread, and people from all over the world start reaching out to you, asking if they can buy one of their own. You're hesitant at first, not wanting to give up your beloved companion, but as demand grows, you realize that you have the opportunity to change the world for the better.
+
+//ASCII IMAGE OF EARTH 
+
+You begin to mass-produce your robot, offering it to people in all corners of the globe. With each new robot that rolls off the assembly line, you feel a sense of joy and fulfillment, knowing that you're making a difference in the world. And as your robots spread out and begin to solve some of the world's biggest problems, you know that you've created something extraordinary.
+Ultimately, you look back on your journey with pride and satisfaction, knowing you've accomplished something incredible. And as you look at your robot, now surrounded by dozens of its brothers and sisters, you know that your creation will continue to make the world a better place for generations."""
+
+good_end = """//
+__/\\\\\\\\\\\\\\\__/\\\___________________________________/\\\\\\\\\\\\\\\_______________________/\\\__        
+ _\///////\\\/////__\/\\\__________________________________\/\\\///////////_______________________\/\\\__       
+  _______\/\\\_______\/\\\__________________________________\/\\\__________________________________\/\\\__      
+   _______\/\\\_______\/\\\_____________/\\\\\\\\____________\/\\\\\\\\\\\______/\\/\\\\\\__________\/\\\__     
+    _______\/\\\_______\/\\\\\\\\\\____/\\\/////\\\___________\/\\\///////______\/\\\////\\\____/\\\\\\\\\__    
+     _______\/\\\_______\/\\\/////\\\__/\\\\\\\\\\\____________\/\\\_____________\/\\\__\//\\\__/\\\////\\\__   
+      _______\/\\\_______\/\\\___\/\\\_\//\\///////_____________\/\\\_____________\/\\\___\/\\\_\/\\\__\/\\\__  
+       _______\/\\\_______\/\\\___\/\\\__\//\\\\\\\\\\___________\/\\\\\\\\\\\\\\\_\/\\\___\/\\\_\//\\\\\\\/\\_ 
+        _______\///________\///____\///____\//////////____________\///////////////__\///____\///___\///////\//__"""
