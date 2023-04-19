2ddb1b71c369923e84e9b8efd8c92490a71e368d
jackpbauer-work <124083011+jackpbauer-work@users.noreply.github.com>
2023-04-19 13:44:34 -0400

    day 3

---
+++ lib/models/good_animal_design.py
@@ -0,0 +1,140 @@
+import time, os
+from time import sleep
+
+1. 
+cat ="""
+
+                                                                                                ___                ___       ___                   
+                                                                                                (   )         .-.  (   )     (   )                  
+                                                                                                | |   ___   ( __)  | |_      | |_      ___  ___    
+                                                                                                | |  (   )  (''") (   __)   (   __)   (   )(   )   
+                                                                                                | |  ' /     | |   | |       | |       | |  | |    
+                                                                                                | |,' /      | |   | | ___   | | ___   | |  | |    
+                                                                                                | .  '.      | |   | |(   )  | |(   )  | '  | |    
+                                                                                                | | `. \     | |   | | | |   | | | |   '  `-' |    
+                                                                                                | |   \ \    | |   | ' | |   | ' | |    `.__. |    
+                                                                                                | |    \ .   | |   ' `-' ;   ' `-' ;    ___ | |    
+                                                                                                (___ ) (___) (___)   `.__.     `.__.    (   )' |    
+                                                                                                                                       ; `-' '    
+                                                                                                                                          .__.'     
+
+
+                                                                                                   ______
+                                                                                                      |             .
+                                                                                                      |           __|___       ^'.           .-
+                                                                                                      |  .          |         / \ '..----..'`/ \
+                                                                                                      |  |  . ,''   |         '  '   /      \  |
+                                                                                                      |  |  | |     |         \ , `''         /
+                                                                                                   ,  .  |  |  `.   |          /  ()    ()    `
+                                                                                                   \  |  |  ;    |  |         /      _         \
+                                                                                                   '-'    `'` ,_,'  |  /     '      \_/         '
+                                                                                                                              `.    ,_        .`'.
+                                                                                             |     __|___.                     `-._/ \,   _.'\   `.
+                                                                                             |       | __|___                    _(__/        `...'^.
+                                                                                             |  ,'   |   |    o             |   /    `\          ,--.'
+                                                                                             ;,'   o |   |    |        .-,  |  |       `.  /    |     ".
+                                                                                             | \   | |   |    | | .-. /  |  |  \_| \_,   ''     '       `
+                                                                                             |  \  | |   |    | |/  | |  |  |    `-'\            `._     \
+                                                                                             |   \ | |  /|    | |   | ;  |  |    '   '.             ' --.'
+                                                                                             |    `| '-' |  / ` |   |  `-|  |   /   '  `'--'       ,.    |
+                                                                                                   `     '-'    |   `-'  |  o  /    |            .'      /
+                                                                                             ____________________________|_____________________________mx
+                                                                                                                           |
+                                                                                                                        \  |
+                                                                                                                        '-'
+
+"""
+
+
+2. 
+dog ="""
+
+                                                                                                            88                          
+                                                                                                            88                          
+                                                                                                            88                          
+                                                                                                   ,adPPYb,88  ,adPPYba,   ,adPPYb,d8  
+                                                                                                   a8"    `Y88 a8"     "8a a8"    `Y88  
+                                                                                                   8b       88 8b       d8 8b       88  
+                                                                                                   "8a,   ,d88 "8a,   ,a8" "8a,   ,d88  
+                                                                                                   `"8bbdP"Y8  `"YbbdP"'   `"YbbdP"Y8  
+                                                                                                                           aa,    ,88  
+                                                                                                                           "Y8bbdP"   
+
+
+                                                                                                   __,'`-,
+                                                                                                _,--'   ,/::.;
+                                                                                             ,-'       ,/::,' `---.___        ___,_
+                                                                                             |       ,:';:/        ;'"`;"`--./ ,-^.;--.
+                                                                                             |:     ,:';,'         '         `.   ;`   `-.
+                                                                                             \:.,:::/;/ -:.                   `  | `     `-.
+                                                                                             \:::,'//__.;  ,;  ,  ,  :.`-.   :. |  ;       :.
+                                                                                                \,',';/O)^. :'  ;  :   '__` `  :::`.       .:' )
+                                                                                                |,'  |\__,: ;      ;  '/O)`.   :::`;       ' ,'
+                                                                                                   |`--''            \__,' , ::::(       ,'
+                                                                                                   `    ,            `--' ,: :::,'\   ,-'
+                                                                                                      | ,;         ,    ,::'  ,:::   |,'
+                                                                                                      |,:        .(          ,:::|   `
+                                                                                                      ::'_   _   ::         ,::/:|
+                                                                                                   ,',' `-' \   `.      ,:::/,:|
+                                                                                                   | : _  _   |   '     ,::,' :::
+                                                                                                   | \ O`'O  ,',   ,    :,'   ;::
+                                                                                                   \ `-'`--',:' ,' , ,,'      ::
+                                                                                                      ``:.:.__   ',-','        ::'
+                                                                                                         `--.__, ,::.         ::'
+                                                                                                            |:  ::::.       ::'
+                                                                                                            |:  ::::::    ,::'
+                                                                                                            |:  :::::::   ::'
+                                                                                                            |:  :::::::   ::'
+                                                                                                            |:  :::::::   ::'
+                                                                                                            |:  :::::::   ::'
+"""
+3.
+elephant ="""
+
+                                                                                                                                                   
+                                                                                      88                        88                                   88
+                                                                                      88                        88                                   88                                    
+                                                                                      88                        88                                   88                                    
+                                                                                      88                        88                                 MM88MMM 
+                                                                                      88                        88                                   88 
+                                                                           ,adPPYba,  88  ,adPPYba, 8b,dPPYba,  88,dPPYba,  ,adPPYYba, 8b,dPPYba,    8c 
+                                                                           a8P_____88 88 a8P_____88 88P'    "8a 88P'    "8a ""     `Y8 88P'   `"8a   88
+                                                                           8PP""""""" 88 8PP""""""" 88       d8 88       88 ,adPPPPP88 88       88   88
+                                                                           "8b,   ,aa 88 "8b,   ,aa 88b,   ,a8" 88       88 88,    ,88 88       88   88        
+                                                                           "8b,   ,aa 88 "8b,   ,aa 88b,   ,a8" 88       88 88,    ,88 88       88   88
+                                                                           ` "Ybbd8"' 88  `"Ybbd8"' 88`YbbdP"'  88       88 `"8bbdP"Y8 88       88   "Y88Y888 
+                                                                                                   88                                              
+                                                                                                   88                 
+
+
+
+                                                                                                            .. ..oooo.....ooo...
+                                                                                                   .odSS4PYYYSSOOXXXXXXXXXOodbgooo.
+                                                                                                   /SSYod$$$$SSOIIPXXXXXXXXXYYP.oo.*b.
+                                                                                                   ($$Yd$$$$SSSOII:XXXXXXXX:IIoSSS$$b.Y,
+                                                                                                   \Yd$$$$SSSOII:XXXXXXXXXX:IIOOSSS$$$b\
+                                                                                                   d$$$$SSSOOI:XP"YXXXXXXXX:IIOOSSSS$$$\
+                                                                                                   Y$$$SSSOOII:XbdXXXXXP"YX:IIOOOSS$$$$)
+                                                                                                   'Y$$$SSSOI:XXXXXXXXXbodX:IIOOSS$$$$$/
+                                                                                                      "Y$$$SSSOI(PoTXXXXXTo)XXIIOOOSS$$$*'
+                                                                                                      ""*Y$S(((PXXXXXXXY))dIIOSSS$$dP'
+                                                                                                         "*'()P;XXXXXXXXY)IIOSSS$P".oS,
+                                                                                                         (S'(P;XXXXXXXP;Y)XXYOP".oSSSSb
+                                                                                                         (S'(P;'XXXXXXX';Y).ooooSSSSSSSS)
+                                                                                                         (S'(P;'XXXXXXP';Y).oSSSSSSSSSSSP
+                                                                                                         (SS'Y);YXXXXX';(Y.oSSSSSSSSSSSSP
+                                                                                                         YSS'Y)'YXXX".(Y.oSSP.SSSSSSSSY
+                                                                                                            YSS'"" XXX""oooSSP.SSSSSSSSY
+                                                                                                            SSSSSS YXXX:SSSSP.SSSSSSSSY
+                                                                                                            SSSSSP  YXb:SSSP.S"SSSSSSP
+                                                                                                            S(OO)S   YXb:SY    )SSSSS
+                                                                                                            SSSSO    )YXb.I    ISSSSP
+                                                                                                            YSSSY    I."YXXb   Y(SS)I
+                                                                                                            )SSS(    dSSo.""*b  YSSSY
+                                                                                                            OooSb   dSSSSP      )SSS(
+                                                                                                                  dSSSY       OooSS
+                                                                                                                  OooSP                 
+
+
+
+"""
--- lib/models/good_robot_story.py
+++ lib/models/good_robot_story.py
@@ -7,12 +7,8 @@ good_robot_ending = """ Great! Your (INTELLIGENT, STRONG, or LIKABLE) (COLOR) ro
 You can imagine your robot being a helpful and approachable companion to anyone interacting with it. 
 After spending weeks working on your robot, pouring all your time and energy into perfecting its design and programming, it’s finally finished; you activate the robot and watch as it comes to life before your eyes. You can't help but feel a sense of pride and satisfaction as you watch your creation move and speak.
 
-// INSERT ASCII IMAGE OF ROBOT HERE
-
 As the days go by, you find yourself becoming more and more attached to your robot. Its the quirkiness and helpful nature make it an indispensable assistant as you continue to learn and grow as a programmer.
 Eventually, news of your robot's incredible abilities and charming personality begins to spread, and people from all over the world start reaching out to you, asking if they can buy one of their own. You're hesitant at first, not wanting to give up your beloved companion, but as demand grows, you realize that you have the opportunity to change the world for the better.
-
-//ASCII IMAGE OF EARTH 
 
 You begin to mass-produce your robot, offering it to people in all corners of the globe. With each new robot that rolls off the assembly line, you feel a sense of joy and fulfillment, knowing that you're making a difference in the world. And as your robots spread out and begin to solve some of the world's biggest problems, you know that you've created something extraordinary.
 Ultimately, you look back on your journey with pride and satisfaction, knowing you've accomplished something incredible. And as you look at your robot, now surrounded by dozens of its brothers and sisters, you know that your creation will continue to make the world a better place for generations."""
@@ -27,3 +23,5 @@ __/\\\\\\\\\\\\\\\__/\\\___________________________________/\\\\\\\\\\\\\\\_____
       _______\/\\\_______\/\\\___\/\\\_\//\\///////_____________\/\\\_____________\/\\\___\/\\\_\/\\\__\/\\\__  
        _______\/\\\_______\/\\\___\/\\\__\//\\\\\\\\\\___________\/\\\\\\\\\\\\\\\_\/\\\___\/\\\_\//\\\\\\\/\\_ 
         _______\///________\///____\///____\//////////____________\///////////////__\///____\///___\///////\//__"""
+
+
